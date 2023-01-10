7801a0cd96960f73d8a8a2e695ca7e5e13fab59c
Nicolas Gonzalez <nicoaugustogonzalez14@gmail.com>
2023-01-10 18:14:41 -0300

    Header created

---
+++ components/Header.tsx
@@ -0,0 +1,23 @@
+import Image from "next/image";
+import Link from "next/link";
+
+export default function Header() {
+    return (
+        <header className="flex items-center justify-between space-x-2 font-boldpx-10 py-5">
+            <div className="flex items-center space-x-2">
+                <Link href="/">
+                    <Image
+                        className="rounded-full"
+                        src="https://links.papareact.com/1m8"
+                        width={50}
+                        height={50}
+                        alt="logo"
+                    />
+                </Link>
+                <h1>THE PAPAFAM</h1>
+            </div>
+
+            <div></div>
+        </header>
+    )
+}
