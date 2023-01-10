82e2cfcce3969f25d60e0eac52dd316e823722cd
Nicolas Gonzalez <nicoaugustogonzalez14@gmail.com>
2023-01-10 01:37:35 -0300

    sanity configs

--- sanity.config.ts
+++ sanity.config.ts
@@ -3,15 +3,16 @@ import { deskTool } from 'sanity/desk';
 import { visionTool } from '@sanity/vision';
 import { schemaTypes } from './schemas';
 
+const projectId = process.env.NEXT_PUBLIC_SANITY_PROJECT_ID!;
+const dataset = process.env.NEXT_PUBLIC_SANITY_DATASET!;
+
 export default defineConfig({
-  name: 'default',
-  title: 'sanity-v3-blog',
-
-  projectId: 'tg2zvob3',
-  dataset: 'production',
-
+  basePath: "/studio",
+  name: 'PAPFAM_Content_Studio',
+  title: 'PAPAFAM Content Studio',
+  projectId,
+  dataset,
   plugins: [deskTool(), visionTool()],
-
   schema: {
     types: schemaTypes,
   },
